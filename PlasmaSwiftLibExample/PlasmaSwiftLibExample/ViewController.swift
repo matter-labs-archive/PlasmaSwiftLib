545cf32dc054f577479eae567dc2a3872d2034ed
BaldyAsh <antongrigorjev2010@gmail.com>
2018-10-25 23:29:43 +0300

    added example project

---
+++ PlasmaSwiftLibExample/PlasmaSwiftLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlasmaSwiftLibExample
+//
+//  Created by Anton Grigorev on 25.10.2018.
+//  Copyright © 2018 The Matter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
