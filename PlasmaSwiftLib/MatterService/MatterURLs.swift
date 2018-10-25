44c89d6f68fd2c346763b33a17e10bb9a6bf2263
BaldyAsh <antongrigorjev2010@gmail.com>
2018-10-25 23:51:31 +0300

    minor fixes

--- PlasmaSwiftLib/MatterService/MatterURLs.swift
+++ PlasmaSwiftLib/MatterService/MatterURLs.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 public class MatterURLs {
+    
+    public init(){}
+    
     static let listUTXOsMainnet: URL = URL(string: "https://plasma.thematter.io/api/v1/listUTXOs")!
     static let listUTXOsTestnet: URL = URL(string: "https://plasma-testnet.thematter.io/api/v1/listUTXOs")!
     static let sendRawTXMainnet: URL = URL(string: "https://plasma.thematter.io/api/v1/sendRawTX")!
